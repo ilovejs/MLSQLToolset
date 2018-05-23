7e28027e45cf3021f7c3e911ede055c704e32ea5
michael.zhuang <michael.zhuang@jaxsta.com>
2018-05-23 10:33:47 +1000

    readme

--- run-codebuff.sh
+++ run-codebuff.sh
@@ -1,28 +1,4 @@
-# java -jar codebuff-1.5.1.jar  \
-#     -rule grammarSpec \
-#     -g org.antlr.codebuff.SQLite \
-#     -corpus corpus/sql/training \
-#     -files sql \
-#     -indent 4 \
-#     -comment LINE_COMMENT \
-#     Ranks.sql
-
-
-# Grammer file locate in: grammars/org/antlr/codebuff    
-
-# -rule compilationUnit \
-# org.antlr.codebuff.tsqlParser.compilationUnit()
-    # -g org.antlr.codebuff.tsql \
-
-# try sqlite
-java -jar target/codebuff-1.5.1.jar \
-    -g org.antlr.codebuff.SQLite \
-    -rule tsql_file \
-    -corpus corpus/sql/training \
-    -files sql -indent 4 -comment LINE_COMMENT \
-    rsc/sql1.sql
-
-#try tsql
+### tsql
 java -jar target/codebuff-1.5.1.jar \
     -g org.antlr.codebuff.tsql \
     -rule tsql_file \
@@ -30,7 +6,7 @@ java -jar target/codebuff-1.5.1.jar \
     -files sql -indent 4 -comment LINE_COMMENT \
     rsc/sql1.sql
 
-## g4 working examples:
+### g4 working examples:
 java -jar target/codebuff-1.5.1.jar \
     -g org.antlr.codebuff.ANTLRv4 \
     -rule grammarSpec \
@@ -38,10 +14,18 @@ java -jar target/codebuff-1.5.1.jar \
     -files g4 -indent 4 -comment LINE_COMMENT \
     rsc/tsql.g4
 
-## java working example:
+### java working example:
 java -jar target/codebuff-1.5.1.jar \
     -g org.antlr.codebuff.Java \
     -rule compilationUnit \
     -corpus corpus/java/training/stringtemplate4  \
     -files java -comment LINE_COMMENT \
     rsc/AnalysisPipeline.java
+
+### sqlite
+java -jar target/codebuff-1.5.1.jar \
+    -g org.antlr.codebuff.SQLite \
+    -rule parse \
+    -corpus corpus/sql/training \
+    -files sql -indent 4 -comment LINE_COMMENT \
+    rsc/sql1.sql
